9160ce22f64b984e2ed555d82761b9b46ff508d9
eremyan <80809901+eremyan@users.noreply.github.com>
2021-05-27 18:39:21 +0300

    Добавил функции проверки кода и формирования кода

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <cctype>
 #include <string>
@@ -19,9 +20,29 @@ public:
     
     //деструктор
     
-    //функция checkTheCode
+    unsigned int checkTheCode(string text, unsigned int code){
+        adler_32 x;
+        unsigned int myCode;
+        myCode=x.setTheFileNameAndGenerateCode(text);
+        if(myCode == code){
+            cout<<"ok"<<endl;
+    } else { 
+        cout<<"not ok"<<endl;
+        }
+        return code;
+        }
     
-    //функция setTheFileNameAndGenerateCode
+    unsigned int setTheFileNameAndGenerateCode(string text){
+        int len = text.size();
+        unsigned int k=0;
+        for(int i=0;i<len;i++){
+            k=text[i];
+            A=(A+k)%65521;
+            B=(B+A)%65521;;
+            }
+            cout<<hex<<B*65536+A<<endl;
+            return (B*65536+A);
+        }
     
     void print() const
     {
@@ -31,23 +52,26 @@ public:
 };
 int main(int argc, char **argv)
 {
+    adler_32 y;
     cout<<"ADLER_32"<<endl;
     string text;
     unsigned op;
     do {
         cout<<"Выбирайте, что будет делать ваш покорный слуга (0-выход, 1-проверить, 2-сгенерировать и задать имя файла): ";
         cin>>op;
-    if (op >0) {
+    if (op>0 && op<=2) {
             cout<<"Вводите текст: ";
             cin>>text;
                 if (op==1) {
-                    cout<<"Проверенный код: "<</*checkTheCode(text)*/<<endl;
+                    unsigned int code;
+                    cout<<"Введите код:";
+                    cin>>code;
+                    cout<<"Проверенный код: "<<y.checkTheCode(text,code)<<endl;
                 } else if (op==2){
-                    cout<<"Сгенерированный код и имя файла"<</*setTheFileNameAndGenerateCode(text)*/<<endl;
+                    cout<<"Сгенерированный код и имя файла:";
+                    y.setTheFileNameAndGenerateCode(text);
                 }
-            } else if (op>2){
-                cout<<"Операция прервана, попался хацкер\n";
-            }
+            } 
     } while (op!=0);
     return 0;
 }
