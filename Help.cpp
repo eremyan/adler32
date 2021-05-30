af109235e1af031783efe130b1c6c968f06e878b
eremyan <80809901+eremyan@users.noreply.github.com>
2021-05-30 11:31:07 +0300

    Ошибочка вышла не тот текст поместил. Теперь правильно

--- Help.cpp
+++ Help.cpp
@@ -1,14 +1,5 @@
-#pragma once
-#include <cstring>
-#include <iostream>
-#include <cctype>
-#include <string>
-#include <iomanip>
-using namespace std;
-class help
+#include "Help.h"
+void help::Helping()
 {
-private:
-    const string Help="Программа Adler-32.\nПрограмма выполняет:\n1.Проверку кода Adler-32 \n2.Формирование кода Adler-32\nПодробнее о том, как работает программа можно узнать по ссылке:\nhttps://ru.wikipedia.org/wiki/Adler-32";
-public:
-    void Helping();
-};
+    cout<<Help<<endl;
+}
