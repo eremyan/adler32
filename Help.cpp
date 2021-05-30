9192fe226da37eebd594507a0088af1bc08b5da9
eremyan <80809901+eremyan@users.noreply.github.com>
2021-05-30 11:29:44 +0300

    Справка

---
+++ Help.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <iomanip>
+using namespace std;
+class help
+{
+private:
+    const string Help="Программа Adler-32.\nПрограмма выполняет:\n1.Проверку кода Adler-32 \n2.Формирование кода Adler-32\nПодробнее о том, как работает программа можно узнать по ссылке:\nhttps://ru.wikipedia.org/wiki/Adler-32";
+public:
+    void Helping();
+};
